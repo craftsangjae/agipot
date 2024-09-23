8c504de7dbece72ef544251d3c935a9bd6f74a97
craftsangjae <craftsangjae@gmail.com>
2024-09-24 00:07:05 +0900

    논문 추가

--- README.md
+++ README.md
@@ -4,4 +4,10 @@
 
 AGIPOT is a codebase for a AGI that can do anything for you. 
 
-## 
+## Setup
+
+1. add your openai api key to the `.env` file
+
+```
+openai_api_key=sk-proj-******
+```
--- agipot/settings.py
+++ agipot/settings.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 
 
 class OpenAISettings(BaseSettings):
-    openai_api_key: str
+    openai_api_key: str # OPENAI_API_KEY also works
 
     class Config:
         env_file = ".env"
